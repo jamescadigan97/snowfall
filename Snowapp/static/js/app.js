74f3150b932ccae1089310c82ab8baa12cea58e4
jamescadigan97 <64744915+jamescadigan97@users.noreply.github.com>
2021-02-09 00:13:02 -0500

    Snowapp and Data

---
+++ Snowapp/static/js/app.js
@@ -0,0 +1,98 @@
+// from data.js
+d3.csv("/static/Data/full_data.csv", function(data){
+    console.log(data)
+
+    var form = d3.select("form")
+    var button = d3.select("button")
+    
+//Set event
+    button.on("click", filter_data);
+    form.on("submit", filter_data);
+
+//Define function 
+    function filter_data(){
+
+    //Prevent refreshing
+    d3.event.preventDefault();
+
+// Filter by  Date    
+    //Select first the value from the first input box
+    var inputElement = d3.select("#datetime")
+    var inputValue = inputElement.property("value")
+
+    //Filter by Date if there is a value
+    if (inputValue.length > 0){
+        snow_filtered = data.filter(data => data.date === inputValue);
+    }
+    //Define unfo_unfiltered even if there is no date
+    else {
+        snow_filtered = data.filter(data => data);
+    }
+
+//Filter by Resort
+    //Select the value from the second input box
+    var inputElement2 = d3.select("#resort")
+    var inputValue2 = inputElement2.property("value")
+
+    //Filter by State if there is a value
+    if (inputValue2.length > 0){
+        snow_filtered = snow_filtered.filter(data => data.resort_name === inputValue2)
+    }
+
+//Filter by Region
+    //Select first the value from the third input box
+    var inputElement3 = d3.select("#region")
+    var inputValue3 = inputElement3.property("value")
+
+    //Filter by City if there is a value
+    if (inputValue3.length > 0){
+        snow_filtered = snow_filtered.filter(data=> data.region === inputValue3);
+    }
+
+//Filter by Country
+    //Select the value from the fourth input box
+    var inputElement4 = d3.select("#country")
+    var inputValue4 = inputElement4.property("value")
+
+    //Filter by Country if there is a value
+    if (inputValue4.length > 0 ){
+        snow_filtered = snow_filtered.filter(data => data.country === inputValue4)
+        console.log("Country")
+        console.log(snow_filtered)
+    }
+
+//Filter by Pass
+    //Select the value from the fifth input box  
+    var inputElement5 = d3.select("#pass")
+    var inputValue5 = inputElement5.property("value")
+
+    //Filter by Pass if there is a value
+    if (inputValue5.length > 0 ){
+        snow_filtered = snow_filtered.filter(data => data.pass === inputValue5)
+    }
+
+    var snow_formated = snow_filtered.map( data => data.date.Date.parse())
+    var snow_sorted = snow_filtered.sort(function (a,b){
+        return a.date - b.date
+    })
+
+    //Clear previous array
+    d3.select("tbody").html("")
+
+    snow_sorted.forEach((data) => {
+
+        //Select location for object 
+        var row = d3.select("tbody").append("tr")
+
+        //Get the entries of the array
+        Object.entries(data).forEach(([key,value]) => {
+        var cell = row.append("td")
+
+        //Add text to HTML elements
+        cell.text(value)
+        });
+    });
+    //Ending for Read function
+    }
+//Ending for Read function
+});
